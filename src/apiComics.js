94044f26488da9195504a177835d0366400e67bc
Laura Portillo <lauraportillo.ark@gmail.com>
2021-10-25 11:35:44 +0200

    clean files

--- src/apiComics.js
+++ src/apiComics.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const products = require('./products');
+const comics = require('./comics');
 
 const app = express();
 
 app.use(express.static('public'));
 
-app.get('/api/products', (req, res) => {
-  res.send(JSON.stringify(products));
+app.get('/api/comics', (req, res) => {
+  res.send(JSON.stringify(comics));
 });
 
 app.listen(8080, console.log('listening 8080'));
